1fa0b6509a98239502fd2720ce8589c1573ef266
eva990417744 <990417744@qq.com>
2016-03-29 00:06:01 +0900

    no message

--- message board.js
+++ message board.js
@@ -2,6 +2,8 @@
  * Created by eva99 on 2016/3/8.
  */
 'use strict';
+var re=new RegExp('token=([0-9a-z]{8}(-[0-9a-z]{4}){3}-[0-9a-z]{12})');
+var token =re.exec(window.location.href);
 function User(){
     this.user={
         id:'',
@@ -20,35 +22,81 @@ function Comment() {
 }
 Comment.prototype=new User();
 var c=new Comment();
-
 function Create() {
     this.CreateParameters={
         text:"",
         parent:0
     };
-    this.createpost=$.ajax('URL: /comment/0',{
-        METHOD: 'POST',
-        data:this.CreateParameters
+    this.createpost=$.ajax({URL:'/comment/0',
+        type:'POST',
+        data:this.CreateParameters,
+        beforeSend: function (request)
+        {
+            request.setRequestHeader('Accept:application/json','x-auth-token:'+token);
+        }
     });
-}
+}//创建留言、回复。
 function Fetch() {
     this.FetchParameters={
-        size:0,
+        size:20,
         page:0
     };
     this.Fetchget=$.ajax({url:'/comment/0',
-        data:this.FetchParameters
+        data:this.FetchParameters,
+        beforeSend: function (request)
+        {
+            request.setRequestHeader('Accept:application/json','x-auth-token:'+token);
+        }
     })
-}
+}//获取留言
 function GET(id) {
     this.id=id;
-    this.GETget=$.ajax({url:'/comment/{'+id+'}'});
-}
+    this.GETget=$.ajax({url:'/comment/{'+id+'}',
+        beforeSend: function (request)
+        {
+            request.setRequestHeader('Accept:application/json','x-auth-token:'+token);
+        }
+    });
+}//获取指定ID的留言信息
 function UPDATE(id) {
     this.id=id;
-    this.UPDATEPATCH =$.ajax({url:'/comment/{'+id+'}', method: 'PATCH'});
-}
+    this.UPDATEPATCH =$.ajax({url:'/comment/{'+id+'}',
+        type: 'PATCH',
+        data:'',
+        beforeSend: function (request)
+        {
+            request.setRequestHeader('Accept:application/json','x-auth-token:'+token);
+        }
+    });
+}//编辑指定ID的留言内容
 function DELETE(id) {
     this.id=id;
-    this.DELETEget=$.ajax({url:'/comment/{'+id+'}',method:'DELETE'});
-}
+    this.DELETEget=$.ajax({url:'/comment/{'+id+'}',type:'DELETE',
+        beforeSend: function (request)
+        {
+            request.setRequestHeader('Accept:application/json','x-auth-token:'+token);
+        }
+    });
+}//删除指定ID的留言
+function clonediv_giveid() {
+    var div=$('.panel.panel-info');
+    var max=3;
+    for (var i=0;i< max;i++)
+    {
+        $("body").append(div.clone());
+    }
+    for(var i=0;i< max+1;i++)
+    {
+        $('.panel.panel-info')[i].id=i;
+    }
+}//复制DIV与给DIV赋值ID
+var mytime=new Date();
+new Vue({
+    el: '#0',
+    data: {
+        avatar:'test.jpg',
+        nickname:'sola',
+        timestamp:myDate.toLocaleDateString(),
+        text:'fuck you'
+    }
+});
