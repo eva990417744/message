b82967377b9a4607023fdb2bfae9d515326308d1
eva990417744 <990417744@qq.com>
2016-03-30 13:49:56 +0900

    no message

--- js/app.js
+++ js/app.js
@@ -4,77 +4,102 @@
 'use strict';
 var re=new RegExp('token=([0-9a-z]{8}(-[0-9a-z]{4}){3}-[0-9a-z]{12})');
 var token =re.exec(window.location.href);
-function User(){
-    this.user={
-        id:'',
-        sex:'',
-        nickname:'',
-        avatar:''
-    };
-}
-function Comment() {
-    this.comment={
-        id:'',
-        parent:'',
-        timestamp:'',
-        text:''
-    };
-}
-Comment.prototype=new User();
 var c=new Comment();
-function Create() {
-    this.CreateParameters={
-        text:"",
-        parent:0
+function Create(inputtext,id) {
+    var CreateParameters={
+        text:inputtext,
+        parent:id
     };
-    this.createpost=$.ajax({URL:'/comment/0',
+    var createpost=$.ajax({URL:'/comment/0',
         type:'POST',
-        data:this.CreateParameters,
+        data:CreateParameters,
         beforeSend: function (request)
         {
             request.setRequestHeader('Accept:application/json','x-auth-token:'+token);
+        },
+        success:function (data) {
+            domUtil.inject(data);
+        },
+        error: function (error) {
+            alert(error);
         }
     });
 }//创建留言、回复。
 function Fetch() {
-    this.FetchParameters={
+    var FetchParameters={
         size:20,
         page:0
     };
-    this.Fetchget=$.ajax({url:'/comment/0',
-        data:this.FetchParameters,
+    var Fetchget=$.ajax({url:'/comment/0',
+        data:FetchParameters,
         beforeSend: function (request)
         {
             request.setRequestHeader('Accept:application/json','x-auth-token:'+token);
+        },
+        success:function (data) {
+            domUtil.injectAll(data.comments);
+        },
+        error: function (error) {
+            console.log(error);
         }
     })
 }//获取留言
 function GET(id) {
     this.id=id;
-    this.GETget=$.ajax({url:'/comment/{'+id+'}',
+    var GETget=$.ajax({url:'/comment/{'+id+'}',
         beforeSend: function (request)
         {
             request.setRequestHeader('Accept:application/json','x-auth-token:'+token);
+        },
+        success:function (data) {
+            domUtil.inject(data);
+        },
+        error: function (error) {
+            console.log(error);
         }
     });
 }//获取指定ID的留言信息
-function UPDATE(id) {
+function UPDATE(text,id) {
     this.id=id;
-    this.UPDATEPATCH =$.ajax({url:'/comment/{'+id+'}',
+    var UPDATEPATCH =$.ajax({url:'/comment/{'+id+'}',
         type: 'PATCH',
-        data:'',
+        data:text,
         beforeSend: function (request)
         {
             request.setRequestHeader('Accept:application/json','x-auth-token:'+token);
+        },
+        success:function (data) {
+            domUtil.inject(data);
+        },
+        error: function (error) {
+            console.log(error);
         }
     });
 }//编辑指定ID的留言内容
 function DELETE(id) {
     this.id=id;
-    this.DELETEget=$.ajax({url:'/comment/{'+id+'}',type:'DELETE',
+    var DELETEget=$.ajax({url:'/comment/{'+id+'}',
+        type:'DELETE',
         beforeSend: function (request)
         {
             request.setRequestHeader('Accept:application/json','x-auth-token:'+token);
+        },
+        error:function () {
+
         }
     });
 }//删除指定ID的留言
+$(document).ready(function(){
+    $('#Createbutton').click(function () {
+        var text=$('#Createtext').text();
+        Create(text,0);
+        location.reload(true);
+    });
+});//Createbuttonclick
+$(document).ready(function(){
+    $('#reCreatebutton').click(function () {
+        var text=$('#reCreatext').text();
+        Create(text,id);
+        location.reload(true);
+    })
+});//reCreatebuttonclick
